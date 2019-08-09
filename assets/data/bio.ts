8147f9feebac6660c12dbc61c4abfca90e608196
shahvru <52763776+shahvru@users.noreply.github.com>
2019-08-09 00:35:30 -0400

    Add files via upload

---
+++ assets/data/bio.ts
@@ -0,0 +1,9 @@
+import {shahvru} from './../../app/shahvru';
+
+export const bio: shahvru={
+    sname: 'Vrushti Shah',
+    slogin: 'shahvru',
+    sid: '991486471',
+    scampus: 'Davis',
+    assign_title: 'Shahvru-A4'
+}
